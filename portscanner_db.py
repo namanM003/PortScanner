f2cf8c3271dd7b9d63e811829eab8f299af83e39
Naman Mittal <namanmittal003@gmail.com>
2015-04-10 01:52:53 -0400

    Corrected database

--- portscanner_db.py
+++ portscanner_db.py
@@ -4,8 +4,8 @@ from datetime import date,datetime
 try:
 	database = sqlite3.connect('portdb')
 	cursor = database.cursor()
-	cursor.execute('''CREATE TABLE IF NOT EXISTS IPINFO(IP TEXT,BLOCK_IP BOOLEAN,PORT BOOLEAN,TIME DATE, PRIMARY KEY(IP,TIME))''')
-	cursor.execute('''CREATE TABLE IF NOT EXISTS PORTDATA(PORT BOOLEAN,IP TEXT, TIME DATE,FOREIGN KEY(IP,TIME) REFERENCES IPINFO(IP,TIME) )''')
+	cursor.execute('''CREATE TABLE IF NOT EXISTS IPINFO(IP TEXT,BLOCK_IP BOOLEAN,PORT BOOLEAN,TIME DATETIME, PRIMARY KEY(IP,TIME))''')
+	cursor.execute('''CREATE TABLE IF NOT EXISTS PORTDATA(PORT BOOLEAN,IP TEXT, TIME DATETIME,FOREIGN KEY(IP,TIME) REFERENCES IPINFO(IP,TIME) )''')
 	database.commit()
 except Exception as e:
 	database.rollback()
