e3131ec56884f3d1d53557da99a846fa9788c3de
Naman Mittal <namanmittal003@gmail.com>
2015-04-15 23:45:06 -0400

    Response field added in database.py

--- datatypes.py
+++ datatypes.py
@@ -1,4 +1,3 @@
-
 class Request():
     def __init__(self, type, ip_addr, ip_subnet, port_start, port_end):
         self.type = type
@@ -6,3 +5,46 @@ class Request():
         self.ip_subnet = ip_subnet
         self.port_start = port_start
         self.port_end = port_end
+    
+    def __init__(self, type, ip_addr, ip_subnet, port_start, port_end, random, date):
+        self.type = type
+        self.ip_addr = ip_addr
+        self.ip_subnet = ip_subnet
+        self.port_start = port_start
+        self.port_end = port_end
+        self.random = random
+        self.date = date
+        
+    def __init__(self, type, ip_addr, ip_subnet, port_start, port_end, date):
+        self.type = type
+        self.ip_addr = ip_addr
+        self.ip_subnet = ip_subnet
+        self.port_start = port_start
+        self.port_end = port_end
+        self.date = date
+        
+    def __init__(self, type, ip_addr, ip_subnet, port_start, port_end, list):
+        self.type = type
+        self.ip_addr = ip_addr
+        self.ip_subnet = ip_subnet
+        self.port_start = port_start
+        self.port_end = port_end
+        self.list = list
+    
+    def __init__(self, type, date, ip_addr, ip_subnet, port_start, port_end):
+        self.type = type
+        self.date = date
+        self.ip_addr = ip_addr
+        self.ip_subnet = ip_subnet
+        self.port_start = port_start
+        self.port_end = port_end
+        
+class Response():
+    def __init__(self, type, date, ip_addr, ip_subnet, port_start, port_end, port_map):
+        self.type = type
+        self.date = date
+        self.ip_addr = ip_addr
+        self.ip_subnet = ip_subnet
+        self.port_start = port_start
+        self.port_end = port_end
+        self.port_map = port_map
